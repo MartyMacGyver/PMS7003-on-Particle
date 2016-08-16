61b4d838edf980e04a17641bcc9d412384df4b47
Martin Falatic <martin@falatic.com>
2016-08-15 21:07:47 -0700

    Bugfix and cleanup

--- pms7003-photon-demo-1/pms7003-photon-demo-1.ino
+++ pms7003-photon-demo-1/pms7003-photon-demo-1.ino
@@ -27,6 +27,10 @@ int detectOff = 0;
 int frameLen = MAX_FRAME_LEN;
 bool inFrame = false;
 char printbuf[256];
+
+const bool DEBUG = false;
+
+
 uint16_t calcChecksum = 0;
 
 struct PMS7003_framestruct {
@@ -49,125 +53,156 @@ struct PMS7003_framestruct {
     uint16_t checksum;
 } thisFrame;
 
+
 void setup() {
     Serial.begin(57600);
-    Serial1.begin(9600);
     delay(1000);
     Serial.println("-- Initializing...");
 }
 
-void loop() {
+
+bool pms7003_read() {
     //  Particle.publish("PMS7003", printbuf, 60, PRIVATE);
     // send data only when you receive data:
-    if (Serial1.available() > 0) {
-        incomingByte = Serial1.read();
-        //Serial.print("I received: ");
-        //Serial.println(incomingByte, HEX);
-        if (!inFrame) {
-            if (incomingByte == 0x42 && detectOff == 0) {
-                frameBuf[detectOff] = incomingByte;
-                thisFrame.frameHeader[0] = incomingByte;
-                calcChecksum += incomingByte;
-                detectOff++;
+    Serial.println("-- Reading PMS7003");
+    Serial1.begin(9600);
+    bool packetReceived = false;
+    while (!packetReceived) {
+        if (Serial1.available() > 32) {
+            int drain = Serial1.available();
+            if (DEBUG) {
+                Serial.print("-- Draining buffer: ");
+                Serial.println(Serial1.available(), DEC);
             }
-            else if (incomingByte == 0x4D && detectOff == 1) {
-                frameBuf[detectOff] = incomingByte;
-                thisFrame.frameHeader[1] = incomingByte;
-                calcChecksum += incomingByte;
-                inFrame = true;
-                detectOff++;
+            for (int i = drain; i > 0; i--) {
+                Serial1.read();
+            }
+        }
+        if (Serial1.available() > 0) {
+            if (DEBUG) {
+                Serial.print("-- Available: ");
+                Serial.println(Serial1.available(), DEC);
+            }
+            incomingByte = Serial1.read();
+            if (DEBUG) {
+                Serial.print("-- READ: ");
+                Serial.println(incomingByte, HEX);
+            }
+            if (!inFrame) {
+                if (incomingByte == 0x42 && detectOff == 0) {
+                    frameBuf[detectOff] = incomingByte;
+                    thisFrame.frameHeader[0] = incomingByte;
+                    calcChecksum = incomingByte; // Checksum init!
+                    detectOff++;
+                }
+                else if (incomingByte == 0x4D && detectOff == 1) {
+                    frameBuf[detectOff] = incomingByte;
+                    thisFrame.frameHeader[1] = incomingByte;
+                    calcChecksum += incomingByte;
+                    inFrame = true;
+                    detectOff++;
+                }
+                else {
+                    Serial.print("-- Frame syncing... ");
+                    Serial.print(incomingByte, HEX);
+                    if (DEBUG) {
+                    }
+                    Serial.println();
+                }
             }
             else {
-                //Serial.println("-- Frame syncing...");
-            }
-        }
-        else {
-            frameBuf[detectOff] = incomingByte;
-            calcChecksum += incomingByte;
-            detectOff++;
-            uint16_t val = frameBuf[detectOff-1]+(frameBuf[detectOff-2]<<8);
-            switch (detectOff) {
-                case 4:
-                    thisFrame.frameLen = val;
-                    frameLen = val + detectOff;
-                    break;
-                case 6:
-                    thisFrame.concPM1_0_CF1 = val;
-                    break;
-                case 8:
-                    thisFrame.concPM2_5_CF1 = val;
-                    break;
-                case 10:
-                    thisFrame.concPM10_0_CF1 = val;
-                    break;
-                case 12:
-                    thisFrame.concPM1_0_amb = val;
-                    break;
-                case 14:
-                    thisFrame.concPM2_5_amb = val;
-                    break;
-                case 16:
-                    thisFrame.concPM10_0_amb = val;
-                    break;
-                case 18:
-                    thisFrame.rawGt0_3um = val;
-                    break;
-                case 20:
-                    thisFrame.rawGt0_5um = val;
-                    break;
-                case 22:
-                    thisFrame.rawGt1_0um = val;
-                    break;
-                case 24:
-                    thisFrame.rawGt2_5um = val;
-                    break;
-                case 26:
-                    thisFrame.rawGt5_0um = val;
-                    break;
-                case 28:
-                    thisFrame.rawGt10_0um = val;
-                    break;
-                case 29:
-                    val = frameBuf[detectOff-1];
-                    thisFrame.version = val;
-                    break;
-                case 30:
-                    val = frameBuf[detectOff-1];
-                    thisFrame.errorCode = val;
-                    break;
-                case 32:
-                    thisFrame.checksum = val;
-                    calcChecksum -= ((val>>8)+(val&0xFF));
-                    break;
-                default:
-                    break;
-            }
-
-            if (detectOff >= frameLen) {
-                sprintf(printbuf, "-- Frame: ");
-                sprintf(printbuf, "%s[%02x %02x] (%04x) ", printbuf,
-                    thisFrame.frameHeader[0], thisFrame.frameHeader[1], thisFrame.frameLen);
-                sprintf(printbuf, "%sCF1=[%04x %04x %04x] ", printbuf,
-                    thisFrame.concPM1_0_CF1, thisFrame.concPM2_5_CF1, thisFrame.concPM10_0_CF1);
-                sprintf(printbuf, "%samb=[%04x %04x %04x] ", printbuf,
-                    thisFrame.concPM1_0_amb, thisFrame.concPM2_5_amb, thisFrame.concPM10_0_amb);
-                sprintf(printbuf, "%sraw=[%04x %04x %04x %04x %04x %04x] ", printbuf,
-                    thisFrame.rawGt0_3um, thisFrame.rawGt0_5um, thisFrame.rawGt1_0um,
-                    thisFrame.rawGt2_5um, thisFrame.rawGt5_0um, thisFrame.rawGt10_0um);
-                sprintf(printbuf, "%sver=%02x err=%02x ", printbuf,
-                    thisFrame.version, thisFrame.errorCode);
-                sprintf(printbuf, "%scsum=%04x %s xsum=%04x", printbuf,
-                    thisFrame.checksum, (calcChecksum == thisFrame.checksum ? "==" : "!="), calcChecksum);
-                Serial.println(printbuf);
-
-                if (calcChecksum == thisFrame.checksum) {
-                    delay(2000);
+                frameBuf[detectOff] = incomingByte;
+                calcChecksum += incomingByte;
+                detectOff++;
+                uint16_t val = frameBuf[detectOff-1]+(frameBuf[detectOff-2]<<8);
+                switch (detectOff) {
+                    case 4:
+                        thisFrame.frameLen = val;
+                        frameLen = val + detectOff;
+                        break;
+                    case 6:
+                        thisFrame.concPM1_0_CF1 = val;
+                        break;
+                    case 8:
+                        thisFrame.concPM2_5_CF1 = val;
+                        break;
+                    case 10:
+                        thisFrame.concPM10_0_CF1 = val;
+                        break;
+                    case 12:
+                        thisFrame.concPM1_0_amb = val;
+                        break;
+                    case 14:
+                        thisFrame.concPM2_5_amb = val;
+                        break;
+                    case 16:
+                        thisFrame.concPM10_0_amb = val;
+                        break;
+                    case 18:
+                        thisFrame.rawGt0_3um = val;
+                        break;
+                    case 20:
+                        thisFrame.rawGt0_5um = val;
+                        break;
+                    case 22:
+                        thisFrame.rawGt1_0um = val;
+                        break;
+                    case 24:
+                        thisFrame.rawGt2_5um = val;
+                        break;
+                    case 26:
+                        thisFrame.rawGt5_0um = val;
+                        break;
+                    case 28:
+                        thisFrame.rawGt10_0um = val;
+                        break;
+                    case 29:
+                        val = frameBuf[detectOff-1];
+                        thisFrame.version = val;
+                        break;
+                    case 30:
+                        val = frameBuf[detectOff-1];
+                        thisFrame.errorCode = val;
+                        break;
+                    case 32:
+                        thisFrame.checksum = val;
+                        calcChecksum -= ((val>>8)+(val&0xFF));
+                        break;
+                    default:
+                        break;
                 }
-
-                detectOff = 0;
-                calcChecksum = 0;
-                inFrame = false;
+    
+                if (detectOff >= frameLen) {
+                    sprintf(printbuf, "PMS7003 ");
+                    sprintf(printbuf, "%s[%02x %02x] (%04x) ", printbuf,
+                        thisFrame.frameHeader[0], thisFrame.frameHeader[1], thisFrame.frameLen);
+                    sprintf(printbuf, "%sCF1=[%04x %04x %04x] ", printbuf,
+                        thisFrame.concPM1_0_CF1, thisFrame.concPM2_5_CF1, thisFrame.concPM10_0_CF1);
+                    sprintf(printbuf, "%samb=[%04x %04x %04x] ", printbuf,
+                        thisFrame.concPM1_0_amb, thisFrame.concPM2_5_amb, thisFrame.concPM10_0_amb);
+                    sprintf(printbuf, "%sraw=[%04x %04x %04x %04x %04x %04x] ", printbuf,
+                        thisFrame.rawGt0_3um, thisFrame.rawGt0_5um, thisFrame.rawGt1_0um,
+                        thisFrame.rawGt2_5um, thisFrame.rawGt5_0um, thisFrame.rawGt10_0um);
+                    sprintf(printbuf, "%sver=%02x err=%02x ", printbuf,
+                        thisFrame.version, thisFrame.errorCode);
+                    sprintf(printbuf, "%scsum=%04x %s xsum=%04x", printbuf,
+                        thisFrame.checksum, (calcChecksum == thisFrame.checksum ? "==" : "!="), calcChecksum);
+                    Serial.println(printbuf);
+                    Particle.publish("Data1", printbuf, 60, PRIVATE);
+                    packetReceived = true;
+                    detectOff = 0;
+                    calcChecksum = 0;
+                    inFrame = false;
+                }
             }
         }
+    }
+    Serial1.end();
+    return (calcChecksum == thisFrame.checksum);
+}
+
+void loop () {
+    if (!pms7003_read()) {
+        delay(4000);
     }
 }
